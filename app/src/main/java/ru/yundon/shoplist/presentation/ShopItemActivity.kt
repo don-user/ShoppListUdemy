4c1b749ee06adeee8c9a65a194ad2226e3ceea56
don-user <don_1303@mail.ru>
2022-03-10 16:41:02 +0300

    add_ShopItemFragment remove activity_shop_item.xml add fragment_shop_item.xml remove ShopItemActivity.kt methods_get_require_in_fragment
    passing parameters via arguments

--- app/src/main/java/ru/yundon/shoplist/presentation/ShopItemActivity.kt
+++ app/src/main/java/ru/yundon/shoplist/presentation/ShopItemActivity.kt
@@ -15,7 +15,7 @@ import ru.yundon.shoplist.domain.ShopItem.Companion.UNDEFINED_ID
 class ShopItemActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityShopItemBinding
-    private lateinit var viewModel: ShopItemViewModel
+//    private lateinit var viewModel: ShopItemViewModel
     private var screenMode = MODE_UNKNOWN
     private var shopItemId = UNDEFINED_ID
 
@@ -24,74 +24,78 @@ class ShopItemActivity : AppCompatActivity() {
         binding = ActivityShopItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
         parseIntent()
-        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-        addTextChangeListeners()
+//        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+//        addTextChangeListeners()
         launchRightMode()
-        observeViewModel()
+//        observeViewModel()
     }
 
-    private fun observeViewModel(){
-        viewModel.errorInputCount.observe(this){
-            val messageCount = if (it) getString(R.string.error_input_count) else null
-            binding.tilCount.error = messageCount
-        }
-        viewModel.errorInputName.observe(this){
-            val messageName = if (it) getString(R.string.error_input_name) else null
-            binding.tilName.error = messageName
-        }
-        viewModel.shouldCloseScreen.observe(this){
-            finish()
-        }
-    }
-    private fun addTextChangeListeners(){
-        binding.edName.addTextChangedListener ( object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.resetErrorInputName()
-            }
-            override fun afterTextChanged(p0: Editable?) {
-            }
-        })
-
-        binding.edCount.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.resetErrorInputCount()
-            }
-            override fun afterTextChanged(p0: Editable?) {
-            }
-        })
-    }
-
+//    private fun observeViewModel(){
+//        viewModel.errorInputCount.observe(this){
+//            val messageCount = if (it) getString(R.string.error_input_count) else null
+//            binding.tilCount.error = messageCount
+//        }
+//        viewModel.errorInputName.observe(this){
+//            val messageName = if (it) getString(R.string.error_input_name) else null
+//            binding.tilName.error = messageName
+//        }
+//        viewModel.shouldCloseScreen.observe(this){
+//            finish()
+//        }
+//    }
+//    private fun addTextChangeListeners(){
+//        binding.edName.addTextChangedListener ( object : TextWatcher {
+//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//            }
+//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                viewModel.resetErrorInputName()
+//            }
+//            override fun afterTextChanged(p0: Editable?) {
+//            }
+//        })
+//
+//        binding.edCount.addTextChangedListener(object : TextWatcher{
+//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//            }
+//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                viewModel.resetErrorInputCount()
+//            }
+//            override fun afterTextChanged(p0: Editable?) {
+//            }
+//        })
+//    }
+//
     private fun launchRightMode(){
-        when(screenMode){
-            MODE_EDIT -> launchEditMode()
-            MODE_ADD-> launchAddMode()
+        val fragment = when(screenMode){
+            MODE_EDIT -> ShopItemFragment.newInstanceEditNewItem(shopItemId)
+            MODE_ADD-> ShopItemFragment.newInstanceAddNewItem()
+            else -> throw RuntimeException("Unknown screen mode $screenMode")
         }
+    supportFragmentManager.beginTransaction()
+        .add(R.id.shop_item_container, fragment)
+        .commit()
     }
-
-    private fun launchEditMode() = with(binding) {
-        viewModel.getShopItemById(shopItemId)
-        viewModel.shopItem.observe(this@ShopItemActivity){
-            edName.setText(it.name)
-            edCount.setText(it.count.toString())
-        }
-        btSave.setOnClickListener {
-            viewModel.editShopItem(edName.text?.toString(), edCount.text?.toString())
-        }
-    }
-
-    private fun launchAddMode() = with(binding) {
-        btSave.setOnClickListener {
-            Toast.makeText(this@ShopItemActivity, "BUTTON", Toast.LENGTH_SHORT).show()
-            viewModel.addShopItem(edName.text?.toString(), edCount.text?.toString())
-            viewModel.shouldCloseScreen.observe(this@ShopItemActivity) {
-                finish()
-            }
-        }
-    }
+//
+//    private fun launchEditMode() = with(binding) {
+//        viewModel.getShopItemById(shopItemId)
+//        viewModel.shopItem.observe(this@ShopItemActivity){
+//            edName.setText(it.name)
+//            edCount.setText(it.count.toString())
+//        }
+//        btSave.setOnClickListener {
+//            viewModel.editShopItem(edName.text?.toString(), edCount.text?.toString())
+//        }
+//    }
+//
+//    private fun launchAddMode() = with(binding) {
+//        btSave.setOnClickListener {
+//            Toast.makeText(this@ShopItemActivity, "BUTTON", Toast.LENGTH_SHORT).show()
+//            viewModel.addShopItem(edName.text?.toString(), edCount.text?.toString())
+//            viewModel.shouldCloseScreen.observe(this@ShopItemActivity) {
+//                finish()
+//            }
+//        }
+//    }
     //проверка параметров которые передаются через интент
     private fun parseIntent(){
         if (!intent.hasExtra(EXTRA_SCREEN_MODE)) throw RuntimeException("Param screen mode is absent")
