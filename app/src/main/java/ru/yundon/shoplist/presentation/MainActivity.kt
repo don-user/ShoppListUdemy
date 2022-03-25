81d611143fddead8dc6cb81620a2266a6fdac415
don-user <don_1303@mail.ru>
2022-03-25 11:35:44 +0300

    data_binding add <layout> in all layout.xml change buildFeatures dataBinding implement dataBinding in ShopListAdapter.kt

--- app/src/main/java/ru/yundon/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/ru/yundon/shoplist/presentation/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.yundon.shoplist.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainer
 import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
